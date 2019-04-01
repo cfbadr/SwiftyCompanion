fd55e1989b3de804281f5457f3f118d4033191b7
Choukri Badr <cfbadr@gmail.com>
2019-04-01 10:52:23 +0200

    Initial Commit

---
+++ Swifty Companion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Companion
+//
+//  Created by Badr Choukri on 01/04/2019.
+//  Copyright © 2019 Badr Choukri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Swifty CompanionTests/Swifty_CompanionTests.swift
@@ -0,0 +1,26 @@
+//
+//  Swifty_CompanionTests.swift
+//  Swifty CompanionTests
+//
+//  Created by Badr Choukri on 01/04/2019.
+//  Copyright © 2019 Badr Choukri. All rights reserved.
+//
+
+import XCTest
+@testable import Swifty_Companion
+
+class Swifty_CompanionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
